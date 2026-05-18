3cc9a148081659119e366301c0cd44c6c2d30c02
V-Lad377 <s34985@pjwstk.edu.pl>
2026-05-18 05:41:06 +0200

    Add helper information about the application

---
+++ src/AppInfo.java
@@ -0,0 +1,7 @@
+public class AppInfo { 
+    public static void printInfo() { 
+        System.out.println("App: pgo-git-start"); 
+        System.out.println("Author: Your Name"); 
+        System.out.println("Course: PGO"); 
+    } 
+} 
